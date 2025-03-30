008345a921da3f207c326bef0aaab8bbb7a41240
akrnwl <moondo28@naver.com>
2025-03-31 04:53:35 +0900

    style : add annotation ( post_api and user_model )

--- lib/app/modules/data/model/user_model.dart
+++ lib/app/modules/data/model/user_model.dart
@@ -3,7 +3,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'user_model.freezed.dart';
 part 'user_model.g.dart';
 
-@freezed
+@freezed // json 직렬화와 관련된 것. freezed를 통해 코드를 간결하게 짤 수 있다.
 abstract class UserModel with _$UserModel {
   const factory UserModel({
     required String id,
